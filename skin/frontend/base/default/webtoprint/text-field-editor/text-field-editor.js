3895123f8c88ada40923750c9bb789da18f5c637
Daniel Corn <d@cundd.net>
2015-10-12 11:50:36 +0200

    Formatted JavaScript files

--- skin/frontend/base/default/webtoprint/text-field-editor/text-field-editor.js
+++ skin/frontend/base/default/webtoprint/text-field-editor/text-field-editor.js
@@ -1,198 +1,200 @@
 (function ($) {
-  var methods = {
-    hide : function () {
-      $editor = this.data('text-field-editor');
+    var methods = {
+        hide: function () {
+            $editor = this.data('text-field-editor');
 
-      if ($editor) {
-        $editor.removeClass('opened');
-        $(document).unbind('click.text-field-editor');
-      }
-    },
+            if ($editor) {
+                $editor.removeClass('opened');
+                $(document).unbind('click.text-field-editor');
+            }
+        },
 
-    move : function (target) {
-      $editor = this.data('text-field-editor');
+        move: function (target) {
+            $editor = this.data('text-field-editor');
 
-      if (!$editor)
-        return;
+            if (!$editor)
+                return;
 
-      $editor
-        .removeClass('opened')
-        .detach()
-        .prependTo(target);
-    }
-  };
-
-  $.fn.text_field_editor = function (method) {
-    var settings = {
-      button_parent: null,
-      colour: '',
-      change: function (data) {}
+            $editor
+                .removeClass('opened')
+                .detach()
+                .prependTo(target);
+        }
     };
 
-    if (methods[method])
-      return methods[method]
-               .apply(this, Array.prototype.slice.call(arguments, 1));
-    else if (typeof method === 'object' || ! method)
-      $.extend(settings, method);
-    else
-      $.error('Method ' +  method +
-              ' does not exist on jQuery.text_field_editor');
+    $.fn.text_field_editor = function (method) {
+        var settings = {
+            button_parent: null,
+            colour: '',
+            change: function (data) {
+            }
+        };
 
-    var $field = this;
+        if (methods[method])
+            return methods[method]
+                .apply(this, Array.prototype.slice.call(arguments, 1));
+        else if (typeof method === 'object' || !method)
+            $.extend(settings, method);
+        else
+            $.error('Method ' + method +
+                ' does not exist on jQuery.text_field_editor');
 
-    var $editor = $('<div class="zp-text-field-editor" />')
-                    .prependTo(settings.button_parent);
+        var $field = this;
 
-    $field.data('text-field-editor', $editor);
+        var $editor = $('<div class="zp-text-field-editor" />')
+            .prependTo(settings.button_parent);
 
-    var $handle = $('<div class="zp-text-field-editor-handle">' +
-                      '<div class="zp-text-field-editor-icon pen" />' +
-                    '</div>').appendTo($editor);
+        $field.data('text-field-editor', $editor);
 
-    var $panel = $('<div class="zp-text-field-editor-panel">' +
-                     '<div class="white-line" />' +
-                   '</div>')
-                   .appendTo($editor);
+        var $handle = $('<div class="zp-text-field-editor-handle">' +
+            '<div class="zp-text-field-editor-icon pen" />' +
+            '</div>').appendTo($editor);
 
-    var $row = $('<div class="zp-text-field-editor-row">' +
-                   '<div class="zp-text-field-editor-icon color-picker" />' +
-                 '</div>').appendTo($panel);
+        var $panel = $('<div class="zp-text-field-editor-panel">' +
+            '<div class="white-line" />' +
+            '</div>')
+            .appendTo($editor);
 
-    var $options = $('<div class="zp-text-field-editor-options" />')
-                     .appendTo($row);
+        var $row = $('<div class="zp-text-field-editor-row">' +
+            '<div class="zp-text-field-editor-icon color-picker" />' +
+            '</div>').appendTo($panel);
 
-    $('<div class="zp-text-field-editor-clear" />').appendTo($row);
+        var $options = $('<div class="zp-text-field-editor-options" />')
+            .appendTo($row);
 
-    var name = 'zp-text-field-editor-colorpicker-'
-                                              + this.attr('name').substring(12);
+        $('<div class="zp-text-field-editor-clear" />').appendTo($row);
 
-    $('<div class="zp-text-field-editor-option">' +
-        '<div><input type="radio" name="' + name + '" value="default" checked="checked" /></div>' +
-        '<div><span>Default</span></div>' +
-      '</div>').appendTo($options);
+        var name = 'zp-text-field-editor-colorpicker-'
+            + this.attr('name').substring(12);
 
-    var $pallet = $('<div class="zp-text-field-editor-icon pallet">' +
-                      '<div class="zp-text-field-editor-color-example" />' +
-                    '</div>');
+        $('<div class="zp-text-field-editor-option">' +
+            '<div><input type="radio" name="' + name + '" value="default" checked="checked" /></div>' +
+            '<div><span>Default</span></div>' +
+            '</div>').appendTo($options);
 
-    var $color_example = $pallet.children();
+        var $pallet = $('<div class="zp-text-field-editor-icon pallet">' +
+            '<div class="zp-text-field-editor-color-example" />' +
+            '</div>');
 
-    var $radio_button = $('<input type="radio" name="' + name + '" value="" />');
+        var $color_example = $pallet.children();
 
-    if (settings.colour) {
-      $color_example.css('backgroundColor', settings.colour);
-      $radio_button.val(settings.colour);
-    }
+        var $radio_button = $('<input type="radio" name="' + name + '" value="" />');
 
-    $('<div class="zp-text-field-editor-option" />')
-      .append($radio_button.wrap('<div />').parent())
-      .append($pallet).appendTo($options);
+        if (settings.colour) {
+            $color_example.css('backgroundColor', settings.colour);
+            $radio_button.val(settings.colour);
+        }
 
-    $handle.click(function () {
-      $(document).unbind('click.text-field-editor');
+        $('<div class="zp-text-field-editor-option" />')
+            .append($radio_button.wrap('<div />').parent())
+            .append($pallet).appendTo($options);
 
-      if ($editor.hasClass('opened'))
-        $editor.removeClass('opened');
-      else {
-        $('div.zp-text-field-editor').removeClass('opened');
+        $handle.click(function () {
+            $(document).unbind('click.text-field-editor');
 
-        var offset = $handle.offset();
-        var position = $handle.position();
+            if ($editor.hasClass('opened'))
+                $editor.removeClass('opened');
+            else {
+                $('div.zp-text-field-editor').removeClass('opened');
 
-        var c = offset.top == position.top && offset.left == position.left
-                  ? offset : position;
+                var offset = $handle.offset();
+                var position = $handle.position();
 
-        $panel.css({
-          top: c.top + $handle.outerHeight() - 1,
-          left: c.left });
+                var c = offset.top == position.top && offset.left == position.left
+                    ? offset : position;
 
-        $editor.addClass('opened');
+                $panel.css({
+                    top: c.top + $handle.outerHeight() - 1,
+                    left: c.left
+                });
 
-        $(document).bind('click.text-field-editor', out_editor_click);
-      }
+                $editor.addClass('opened');
 
-      return false;
-    });
+                $(document).bind('click.text-field-editor', out_editor_click);
+            }
 
-    $('input', $row).change(function () {
-      var value = $(this).val();
+            return false;
+        });
 
-      if (!value)
-        $radio_button.colorpicker('open');
-      else if (value == 'default')
-        _change('color', undefined);
-      else
-        _change('color', value);
-    });
+        $('input', $row).change(function () {
+            var value = $(this).val();
 
-    var color_picker_on = false;
+            if (!value)
+                $radio_button.colorpicker('open');
+            else if (value == 'default')
+                _change('color', undefined);
+            else
+                _change('color', value);
+        });
 
-    $radio_button.colorpicker({
-      color: '804080',
-      inline: false,
-      layout: {
-        //Left, Top, Width, Height (in table cells)
-        map:     [0, 0, 1, 5],
-        bar:     [1, 0, 1, 5],
-        preview: [2, 0, 1, 1],
-        rgb:     [2, 2, 1, 1],
-        hex:     [2, 3, 1, 1],
-        cmyk:    [3, 2, 1, 2],
-      },
-      parts: [
-        'switcher', 'header', 'map', 'bar', 'hex', 'rgb', 'cmyk', 'preview',
-        'footer'
-      ],
-      part: {
-        map:  { size: 128 },
-        bar:  { size: 128 }
-      },
-      altField: $color_example,
-      showOn: 'alt',
-      title: ' ',
-      revert: true,
-      showCloseButton: false,
-      colorFormat: ('#HEX'),
+        var color_picker_on = false;
 
-      open: function () {
-        color_picker_on = true;
-      },
+        $radio_button.colorpicker({
+            color: '804080',
+            inline: false,
+            layout: {
+                //Left, Top, Width, Height (in table cells)
+                map: [0, 0, 1, 5],
+                bar: [1, 0, 1, 5],
+                preview: [2, 0, 1, 1],
+                rgb: [2, 2, 1, 1],
+                hex: [2, 3, 1, 1],
+                cmyk: [3, 2, 1, 2],
+            },
+            parts: [
+                'switcher', 'header', 'map', 'bar', 'hex', 'rgb', 'cmyk', 'preview',
+                'footer'
+            ],
+            part: {
+                map: {size: 128},
+                bar: {size: 128}
+            },
+            altField: $color_example,
+            showOn: 'alt',
+            title: ' ',
+            revert: true,
+            showCloseButton: false,
+            colorFormat: ('#HEX'),
 
-      close: function (colpkr) {
-        color_picker_on = false;
-      },
+            open: function () {
+                color_picker_on = true;
+            },
 
-      ok: function (e, data) {
-        _change('color', data.formatted);
-      }
-    });
+            close: function (colpkr) {
+                color_picker_on = false;
+            },
 
-    function out_editor_click (event) {
-      if (color_picker_on)
-        return;
+            ok: function (e, data) {
+                _change('color', data.formatted);
+            }
+        });
 
-      var editor = $editor.get(0);
-      var child_parent = $(event.target)
-                          .parents('div.zp-text-field-editor')
-                          .get(0);
+        function out_editor_click(event) {
+            if (color_picker_on)
+                return;
 
-      if (!((event.target == editor) || (child_parent == editor))) {
-        $handle.click();
-      }
-    }
+            var editor = $editor.get(0);
+            var child_parent = $(event.target)
+                .parents('div.zp-text-field-editor')
+                .get(0);
 
-    function _change (name, value) {
-      if (value === undefined)
-        $editor.removeClass('state-changed');
-      else
-        $editor.addClass('state-changed');
+            if (!((event.target == editor) || (child_parent == editor))) {
+                $handle.click();
+            }
+        }
 
-      var data = {};
-      data[name] = value
+        function _change(name, value) {
+            if (value === undefined)
+                $editor.removeClass('state-changed');
+            else
+                $editor.addClass('state-changed');
 
-      settings.change(data);
-    }
+            var data = {};
+            data[name] = value
 
-    return this;
-  };
+            settings.change(data);
+        }
+
+        return this;
+    };
 })(jQuery);
